233da440b0ba56ca667134a5ace3ebb120e7ab15
rbarros <ricardofbarros@hotmail.com>
2014-08-04 19:03:53 +0100

    Update spritrel src and brain example

--- src/spritrel.js
+++ src/spritrel.js
@@ -1,73 +1,88 @@
-function spritrel(elem, options) {
+(function ( $ ) {
     // Error messages
     var errorMissingElem = 'You need to give a valid Id or Class of an element',
         errorMissingOptions = 'You need to set the required params in options';
+    
+    var spritrel = {
+        elems : []
+    };
+    var root = this;
 
-    // First check - see if var elem is set
-    if(typeof elem === 'undefined') {
-        throw new Error(errorMissingElem);
-    }
+    spritrel.create = function(elem, options) {
+        // First check - see if var elem is set
+        if(typeof elem === 'undefined') {
+            throw new Error(errorMissingElem);
+        }   
+        
+        // Get the selector type of element and the name of element
+        var selector = elem.substr(0,1),
+            elementName = elem.substr(1),
+            classFlag = false,
+            element;
 
-    // Get the selector type of element and the name of element
-    var selector = elem.substr(0,1),
-        elementName = elem.substr(1),
-        classFlag = false,
-        element;
+        
+        // Second check - check if elem is a valid DOM Element
+        // as class or an id
+        if(selector === '.') {
+            element = document.getElementsByClassName(elementName);
+            classFlag = true;
+        } else if(selector === '#') {
+            element = document.getElementById(elementName);
+        } else {
+            throw new Error(errorMissingElem);
+        }
 
-    // Second check - check if elem is a valid DOM Element
-    // as class or an id
-    if(selector === '.') {
-        element = document.getElementsByClassName(elementName);
-        classFlag = true;
-    }
-    else if(selector === '#') {
-        element = document.getElementById(elementName);
-    } else {
-        throw new Error(errorMissingElem);
-    }
+        // Third check - Check if required options exist
+        if(typeof options === 'undefined') {
+            throw new Error(errorMissingOptions);
+        }
+        if(typeof options.cols   === 'undefined')
+        {
+            throw new Error(errorMissingOptions);
+        }
 
-    // Third check - Check if required options exist
-    if(typeof options === 'undefined') {
-        throw new Error(errorMissingOptions);
-    }
-    if(typeof options.cols   === 'undefined' ||
-       typeof options.img    === 'undefined' ||
-       typeof options.width  === 'undefined' ||
-       typeof options.height === 'undefined') {
-        throw new Error(errorMissingOptions);
-    }
+        options.height = element.naturalHeight / options.rows;
+        options.width = element.naturalWidth / options.cols;     
 
-    // Set element style
-    element.style.background =  'transparent url('+options.img+') 0 0 no-repeat';
-    element.style.width =  options.width+'px';
-    element.style.height =  options.height+'px';
+        // Check if fps is set, default 9
+        if(typeof options.fps === 'undefined') {
+            options.fps = 9;
+        }
 
-    // Check if rows is set, default 1
-    if(typeof options.rows   === 'undefined') {
-        options.rows = 1;
-    }
+        // Simple math to translate fps to interval for the loop
+        var interval = 1000 / options.fps;
+        var loop;
+        
+        // Construct div
+        var imgDiv = document.createElement("DIV");
+        imgDiv.style.background =  'transparent url('+element.src+') 0 0 no-repeat';
+        imgDiv.style.width =  options.width+'px';
+        imgDiv.style.height =  options.height+'px';
+        imgDiv.id = elementName
+        
+        // Replace img with div in DOM
+        element.parentNode.replaceChild(imgDiv, element);
+        
+        // Pass the new reference of the element
+        element = imgDiv;
 
-    // Check if fps is set, default 9
-    if(typeof options.fps === 'undefined') {
-        options.fps = 9;
-    }
+        /**
+         * Construct object
+         * @type type
+         */
+        var Spritrel = {};
 
-    // Simple math to translate fps to interval for the loop
-    var interval = 1000 / options.fps;
-    var loop;
-
-    var Spritrel = {
         // Flow control fn
-        start : function() {
+        Spritrel.start = function() {
             var countCols = 0;
             var countRows = 0;
-
+            
             loop = setInterval(function() {
                 var posX = countCols * options.width;
                 var posY = countRows * options.height;
                 element.style.backgroundPosition =  '-'+posX+'px -'+posY+'px';
 
-                if(countRows >= (options.rows - 1)) {
+                if(countRows >= (options.rows - 1) && countCols >= (options.cols - 1)) {
                     countCols = 0;
                     countRows = 0;
                 } else {
@@ -78,32 +93,50 @@ function spritrel(elem, options) {
                         countCols++;
                     }
                 }
-            },interval);
-        },
-        stop : function() {
-            clearInterval(loop);
-        },
-        goTo : function(sprite) {
+            },interval);                
+        };
 
-        },
-        changeDirection : function() {
+        Spritrel.stop = function() {
+            clearInterval(loop);                     
+        };
+        
+        Spritrel.resume = function() {
+            
+            
+        };
+        
+        
+        Spritrel.goTo = function() {
 
-        },
-        changeSpeed : function(fps) {
+        };
 
-        },
-        flip : function() {
 
-        },
+        Spritrel.changeDirection = function() {
+
+        };
+
+        Spritrel.changeSpeed = function(fps) {
+
+        };
+
+        Spritrel.flip = function() {
+
+
+        };
 
         // Event control fn
-        on : function(sprite, cb) {
+        Spritrel.on = function(sprite, cb) {
 
-        },
-        onEach : function(cb) {
+        };
 
-        }
-    };
+        Spritrel.onEach = function(cb) {
 
-    return Spritrel
-}
+        };   
+        
+        spritrel.elems.push(Spritrel);
+        
+        return Spritrel;
+    };  
+    
+    root.spritrel = spritrel;
+}());
